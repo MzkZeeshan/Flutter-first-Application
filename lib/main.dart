c5c5378f950b23c8e75ec5aea5dbb043aa367f22
MzkZeeshan <you@example.com>
2019-12-06 17:07:29 +0500

    change colors

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  final barColor = const Color(0XFF019031);
+  final backgroundColor = const Color(0XFFA3CB37);
+
+  var app = MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Mzk Zeeshan Khan"),
+        backgroundColor: barColor,
+      ),
+      backgroundColor: backgroundColor,
+    ),
+  );
+
+  runApp(app);
+}
